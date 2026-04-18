3a61cf1711ae5f69491d4f5228eea4d5cf1936bb
Mila San <melicsanchez8@gmail.com>
2026-04-19 08:30:41 +0900

    Base

---
+++ Basesdedatos2/script.sql
@@ -0,0 +1,6 @@
+-- ORDER BY, LIMIT, OFSET
+ORDER BY: Ordenar los resultados por una o más columnas
+ASC: Ordenar asecendentemente
+DESC: Ordenar descendentemente
+LIMIT n: Muestra los primeros n resultados
+OFSET: Salta los n resultados
